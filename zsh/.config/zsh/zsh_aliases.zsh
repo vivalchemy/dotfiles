ea779029149959a25f60849603f763b9e15c729c
vivalchemy <vivalchemy.social+github@gmail.com>
2024-06-18 19:03:42 +0530

    some quality of life improvements

--- zsh/.config/zsh/zsh_aliases.zsh
+++ zsh/.config/zsh/zsh_aliases.zsh
@@ -19,6 +19,7 @@ checkError(){
 alias hg="sed 's/.*;//g' $HISTFILE | fzf | wl-copy"
 alias fzfview='fzf --preview "bat --color=always --style=numbers --line-range=:500 {}"'
 alias fzfv='fzf --preview "bat --color=always --style=numbers --line-range=:500 {}"'
+# alias fzf='fzf --tmux 70%'
 
 # Neovim
 alias nv='nvim $(fzfview)'
@@ -90,3 +91,21 @@ alias fc-view='view $(fc-list | fzf | sed "s/:.*//")'
 
 alias lg='lazygit'
 alias ld='lazydocker'
+
+# tmux
+alias tmf='tmuxifier'
+## sessions
+alias tmx='tmuxifier load-session $(tmuxifier list-sessions | fzf --height=10 --layout=reverse)' # cause my brain is dumb
+alias tmxn='tmuxifier new-session $(gum input --placeholder "Tmux Session Name")' # cause my brain is dumb
+alias tmxe='tmuxifier edit-session $(tmuxifier list-sessions | fzf --height=10 --layout=reverse)' # cause my brain is dumb
+alias tmxl='tmuxifier list-sessions' # cause my brain is dumb
+alias tmxd='rm $TMUXIFIER_LAYOUT_PATH/$(tmuxifier list-sessions | fzf --preview "bat --color=always --style=numbers --line-range=:500 $TMUXIFIER_LAYOUT_PATH/{}.session.sh").session.sh'
+# rm $TMUXIFIER_LAYOUT_PATH/$(tmuxifier list-sessions | fzf --preview "bat --color=always $TMUXIFIER_LAYOUT_PATH/{}.session.sh").session.sh
+
+## windows
+alias tmxw='tmuxifier load-window $(tmuxifier list-windows | fzf --height=10 --layout=reverse)' # cause my brain is dumb
+alias tmxnw='tmuxifier new-window $(gum input --placeholder "Tmux Session Name")' # cause my brain is dumb
+alias tmxew='tmuxifier edit-window $(tmuxifier list-windows | fzf --height=10 --layout=reverse)' # cause my brain is dumb
+alias tmxlw='tmuxifier list-windows' # cause my brain is dumb
+alias tmxdw='rm $TMUXIFIER_LAYOUT_PATH/$(tmuxifier list-windows | fzf --preview "bat --color=always --style=numbers --line-range=:500 $TMUXIFIER_LAYOUT_PATH/{}.window.sh").window.sh'
+
