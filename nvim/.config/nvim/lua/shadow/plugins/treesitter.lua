f72e1c63b0dd170a1bf70b2f43215bd5ce0508d7
Vivian Ludrick <test@test.com>
2024-10-07 02:38:46 +0530

    added textobject and incremental select support

--- nvim/.config/nvim/lua/shadow/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/shadow/plugins/treesitter.lua
@@ -1,6 +1,9 @@
 return { -- Highlight, edit, and navigate code
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter-textobjects",
+	},
 	opts = {
 		ensure_installed = {
 			"bash",
@@ -32,6 +35,58 @@ return { -- Highlight, edit, and navigate code
 			enable = true,
 			disable = {
 				"ruby",
+			},
+		},
+		textobjects = {
+			select = {
+				enable = true,
+
+				-- Automatically jump forward to textobj, similar to targets.vim
+				lookahead = true,
+
+				keymaps = {
+					-- You can use the capture groups defined in textobjects.scm
+					["af"] = { query = "@function.outer", desc = "Select around a function call" },
+					["if"] = { query = "@function.inner", desc = "Select inner part of a function call" },
+					["ac"] = { query = "@class.outer", desc = "Select around a class" },
+					-- You can optionally set descriptions to the mappings (used in the desc parameter of
+					-- nvim_buf_set_keymap) which plugins like which-key display
+					["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+					["ip"] = { query = "@parameter.inner", desc = "Select inner part of a parameter region" },
+					-- You can also use captures from other query groups like `locals.scm`
+					-- ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+				},
+				-- You can choose the select mode (default is charwise 'v')
+				--
+				-- Can also be a function which gets passed a table with the keys
+				-- * query_string: eg '@function.inner'
+				-- * method: eg 'v' or 'o'
+				-- and should return the mode ('v', 'V', or '<c-v>') or a table
+				-- mapping query_strings to modes.
+				selection_modes = {
+					["@parameter.outer"] = "v", -- charwise
+					["@function.outer"] = "V", -- linewise
+					["@class.outer"] = "<c-v>", -- blockwise
+				},
+				-- If you set this to `true` (default is `false`) then any textobject is
+				-- extended to include preceding or succeeding whitespace. Succeeding
+				-- whitespace has priority in order to act similarly to eg the built-in
+				-- `ap`.
+				--
+				-- Can also be a function which gets passed a table with the keys
+				-- * query_string: eg '@function.inner'
+				-- * selection_mode: eg 'v'
+				-- and should return true or false
+				include_surrounding_whitespace = true,
+			},
+		},
+		incremental_selection = {
+			enable = true,
+			keymaps = {
+				init_selection = "<CR>",
+				scope_incremental = "<CR>",
+				node_incremental = "<TAB>",
+				node_decremental = "<S-TAB>",
 			},
 		},
 	},
