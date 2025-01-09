f87bfbdb0ed66cbee297fc900b6d3abe01465460
Vivian Ludrick <116781909+vivalchemy@users.noreply.github.com>
2025-01-09 19:41:04 +0530

    no need to unneccesarily use it

--- roles/neovim/files/config/lua/plugins/executor.lua
+++ roles/neovim/files/config/lua/plugins/executor.lua
@@ -15,7 +15,7 @@ return {
 			vim.api.nvim_command("write")
 			executor.commands.run()
 			executor.commands.show_detail()
-			vim.cmd("lua Snacks.notifier.hide()")
+			Snacks.notifier.hide()
 		end, {})
 		vim.keymap.set("n", "<leader>tx", require("executor").commands.toggle_detail, {})
 		vim.keymap.set("n", "<leader>rx", require("executor").commands.set_command, {})
