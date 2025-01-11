d7506ae3536f5e23d4881277783aa2b641697a91
Vivian Ludrick <116781909+vivalchemy@users.noreply.github.com>
2025-01-11 17:56:33 +0530

    fixed the starship issue when relaunching the shell

--- roles/zsh/files/config/plugins/native/starship.zsh
+++ roles/zsh/files/config/plugins/native/starship.zsh
@@ -1,1 +1,7 @@
+# fixes 'starship_zle-keymap-select-wrapped:1: maximum nested function level reached; increase FUNCNEST?'
+if [[ "${widgets[zle-keymap-select]#user:}" == "starship_zle-keymap-select" || \
+      "${widgets[zle-keymap-select]#user:}" == "starship_zle-keymap-select-wrapped" ]]; then
+    zle -N zle-keymap-select "";
+fi
+
 eval "$(starship init zsh)"
