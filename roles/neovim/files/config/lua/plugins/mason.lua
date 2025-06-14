38200ce95386041c57e3864ea814ccf3acf5b3a8
Vivian Ludrick <116781909+vivalchemy@users.noreply.github.com>
2025-06-14 23:10:56 +0530

    moved the mason plugin source to the correct repo. updated the yazi packages

--- roles/neovim/files/config/lua/plugins/mason.lua
+++ roles/neovim/files/config/lua/plugins/mason.lua
@@ -1,8 +1,8 @@
 return {
-	"williamboman/mason.nvim",
+	"mason-org/mason.nvim",
 	-- cmd = "Mason",
 	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
+		"mason-org/mason-lspconfig.nvim",
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
 		{
 			"saghen/blink.cmp",
