a4d8351b744a3182bc9431cd91694fca9945c9aa
Vivian Ludrick <116781909+vivalchemy@users.noreply.github.com>
2025-10-15 18:43:05 +0530

    removed uwsm changed wallpapers etc etc

---
+++ roles/zsh/files/config/plugins/external/kubernetes.zsh
@@ -0,0 +1,16 @@
+alias k='kubectl'
+
+alias ka='kubectl apply'
+alias kaf='kubectl apply -f'
+
+alias kg='kubectl get'
+alias kga='kubectl get all'
+alias kgp='kubectl get pods'
+alias kgs='kubectl get services'
+alias kgd='kubectl get deployments'
+alias kgrs='kubectl get replicasets'
+alias kgrc='kubectl get replicationcontrollers'
+
+alias kd='kubectl describe'
+
+alias kl='kubectl logs'
